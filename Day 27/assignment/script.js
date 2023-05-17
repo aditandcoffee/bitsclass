ec8a69770af3395823eb866e6f0016fa59dddd69
Adit Adhikari <aditvisuals@gmail.com>
2023-05-17 17:15:22 +0545

    assignment of cmsa

---
+++ Day 27/assignment/script.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper(".myswiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+Fancybox.bind("[data-fancybox]", {
+  
+});
+
+
